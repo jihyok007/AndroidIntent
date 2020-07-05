eb1f16fa95fcb2a21acdc7bb251733228caaac46
Jihyok Choi <jihyok007@gmail.com>
2020-07-05 15:16:44 +0900

    문자 전송 화면 이동하는 기능 -putExtra "sms_body"로 내용 미리 작성 가능

--- app/src/main/java/kr/co/tjoeun/androidintent/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/androidintent/MainActivity.kt
@@ -29,10 +29,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         callBtn.setOnClickListener {
-       val inputPhone = phoneNumEdt.text.toString()
+            val inputPhone = phoneNumEdt.text.toString()
+
             val myUri = Uri.parse(uriString: "tel:${inputPhone}")
             val myIntent = Intent(Intent.ACTION_CALL, myUri)
             startActivity(myIntent)
+        }
+
+        sendSmsBtn.setOnClickListener {
+//            입력한 폰번/ 문자 내용 받아오기
+            val inputPhone = phoneNumEdt.text.toString()
+            val content = smsContentEdt.text.toString()
+//            전화번호 uri로 변경
+            val myUri = Uri.parse("smsto:${inputPhone}")
+//            문자 전송 화면으로 이동
+            val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
+//            문자 화면 이동시 데이터 첨부
+            myIntent.putExtra("sms_body",content)
+            startActivity(myIntent)
+        }
 
         }
     }
